afeaa8822c48c465dc04c46b40f3049a0622e33c
Simone0401 <argento.1939925@studenti.uniroma1.it>
2023-05-10 17:25:27 +0200

    Giustificazione testo completa
    Implementata la versione funzionante per qualsiasi lunghezza L della riga della giustificazione completa. Questa versione funziona momentaneamente solo su codifica ASCII

--- First implementation test/main.c
+++ First implementation test/main.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <signal.h>
+#include <unistd.h>
 #define N 1000
 
 /*
@@ -9,209 +11,101 @@
 */
 
 /*
- * Si è deciso di creare la lista di caratteri entranti in una riga al fine di non sovraccaricare la memoria
+ * TODO: conto prima le parole perché reallocando l'array mano a mano potrei avere aree di memoria diverse e perdre tutti i nodi già inseriti se non li copio di volta in volta, questo crea una complessità computazionale maggiore. La funzione realloc non garantisce che l'area di memoria rimanga sempre la stessa
  */
 
+
+/**
+ * La struttura definisce il modo in cui verrà memorizzata una parola
+ * @var parola è la stringa da memorizzare come una parola. Consiste in un array.
+ * @var size indica la dimensione della stringa memorizzata. è di tipo unsigned short int perchè le parole devono avere una lunghezza > 0
+ */
 typedef struct {
     char *parola;
     unsigned short size;
-    struct Parola* next;
 } Parola;
 
-Parola* componiLista(Parola*, Parola**, char*, int, int*);
-Parola* creaNodo(char*);
-bool isEscapeChar(char);
-Parola* pop(Parola**);
-Parola* push(Parola*, Parola*, Parola**);
+/**
+ * La struttura definisce il modo in cui verrà memorizzata una riga da stampare
+ * @var riga è la riga, sotto forma di stringa, da stampare
+ * @var size è la dimensione delle stringa da stampare
+ * @var next è un puntatore che referenzia alla prossima riga da stampare
+ */
+typedef struct {
+    char *riga;
+    unsigned short size;
+    struct Riga *next;
+} Riga;
+
+void build_array(Parola** parole, int n_word, char *buff);
+void free_buff(void* buff, int size);
+bool isEscapeChar(char c);
+Parola* crea_nodo(char* buff);
+int count_words(char* buff);
+void full_Justyfy(Parola** parole, int n_words, int L, Riga *testa, Riga *coda, int *nRighe);
+bool check_word_length(char *parola, int L);
+void left_justify(Parola** parole, int i, int j, int numbersOfSpaces, Riga *testa, Riga *coda, int *nRighe);
+void middle_justify(Parola** parole, int i, int j, int numberOfSpaces, Riga *testa, Riga *coda, int *nRighe);
 
 int main() {
     char buff[N] = "string.h e' l'header file della libreria standard del C che contiene definizioni di macro, costanti e dichiarazioni di funzioni e tipi usati non solo nella manipolazione delle stringhe ma anche nella manipolazione della memoria.\n\0";
+    Parola** parole = NULL;
+    int n_parole;
     int L;
-    int i = 0;
+    Riga* testa_righe = NULL;
+    Riga* coda_righe = NULL;
+    int nRighe = 0;
 
     printf("Inserisci larghezza di una riga: ");
     scanf("%d", &L);
 
-    Parola *testa = NULL;
-    Parola *coda = NULL;
+    n_parole = count_words(buff);
+    parole = (Parola**) malloc(sizeof(Parola)*n_parole);
 
-    while (i < strlen(buff)) {
-        Parola* testa_riga = NULL;
-        Parola* coda_riga = NULL;
-        int k;
-        testa = componiLista(testa, &coda, &(buff[i]), L, &k);
-        /* L'indice viene spostato indietro nel caso si sono lette lettere ma la parola non era finita */
-        i += k;
-        if(buff[i] == ' ') {
-            i++;
-        }
-        int caratteri = 0, n_words = 0, effective_c = 0;
-        bool printable = false;
-        while (!printable) {
-            Parola* nodo = pop(&testa);
-            /* Se ancora non raggiungo L caratteri stampabili accodo la parola nella lista delle parole della riga */
-            if (nodo != NULL && caratteri + nodo->size < L) {
-                caratteri += nodo->size + 1;            // TODO: controllare il comportamento qui. Potrebbe accodare uno spazio alla fine dell'ultima parola della riga
-                effective_c += nodo->size;
-                testa_riga = push(nodo, testa_riga, &coda_riga);
-                n_words++;
-            }
-            /* Se i caratteri coprono perfettamente la lunghezza L significa che tutte le parole tra loro hanno uno spazio */
-            else if ( (nodo == NULL && caratteri == L) || (nodo != NULL && caratteri + nodo->size == L) ) {
-                if (nodo != NULL && caratteri + nodo->size == L) {
-                    n_words++;
-                    effective_c += nodo->size;
-                }
-                /* Stampiamo tutte le parole con uno spazio tra esse */
-                int space_to_place = L - effective_c;
-                int space_each_world = space_to_place / (n_words-1);
-                while (n_words > 0) {
-                    Parola *p = pop(&testa_riga);
-                    if (n_words == 1 && space_to_place == 1) {
-                        printf(" ");
-                        space_to_place--;
-                    }
-                    printf("%s", p->parola);
-                    if (n_words != 1) {
-                        for (int j = 0; j < space_each_world; ++j) {
-                            printf(" ");
-                            space_to_place--;
-                        }
-                    }
-                    else {
-                        printf("\n");
-                    }
-                    free(p);
-                    n_words--;
-                }
-                printable = true;
-            }
-            /* Se i caratteri sono maggiori rispetto alla lunghezza L della riga significa che dovrà esserci una giustificazione */
-            else {
-                caratteri--;
-                /* Reinserisco la parola all'interno della lista altrimenti la perderei */
-                testa = push(nodo, testa, &coda);
-                /* Potrei avere una sola parola nella lista perchè quella che stavo leggendo mi fa sforare la lunghezza L */
-                if (n_words == 1) {
-                    Parola *p = pop(&testa_riga);
-                    printf("%s ", p->parola);
-                    for (int j = 0; j < L-p->size; ++j) {
-                        printf(" ");
-                    }
-                    printf("\n");
-                    free(p);
-                    n_words--;
-                }
-                else {
-                    int space_to_place = L - caratteri;
-                    int space_each_world = space_to_place / (n_words-1);
-                    while(n_words > 1) {
-                        Parola *p = pop(&testa_riga);
-                        printf("%s ", p->parola);
-                        for(int i = 0; i < space_each_world; i++) {
-                            printf(" ");
-                        }
-                        space_to_place -= space_each_world;
-                        free(p);
-                        n_words--;
-                    }
-                    Parola *p = pop(&testa_riga);
-                    for (int j = 0; j < space_to_place; ++j) {
-                        printf(" ");
-                    }
-                    printf("%s\n", p->parola);
-                    free(p);
-                    n_words--;
-                }
-                printable = true;
-            }
-        }
+    build_array(parole, n_parole, buff);
+    full_Justyfy(parole, n_parole, L);
 
-    }
 
-    return 0;
 }
 
 /**
- * Crea una lista di parole da un buffer. Dal buffer verranno letti massimo L caratteri
- * @param buffer buffer dal quale leggere i caratteri
- * @param L massimo numero di caratteri da leggere. Rappresenta la lunghezza della riga
- * @param k eventuali caratteri letti in più
- * @return la testa della lista
+ * Inserisci all'interno di un array di puntatori a struct 'Parola' una quantità determinata di parole
+ * @param parole array dove inserire le varie strutture di tipo 'Parola'
+ * @param n_words numero di elementi dell'array (corrisponde al numero di parole)
+ * @param buff il buffer dal quale leggere le parole
  */
-Parola* componiLista(Parola* testa, Parola** coda, char* buffer, int L, int* k) {
-    int caratteri = 0;          // numero di caratteri letti fino ad un certo istante
-    int is_word = 0, i = 0, j = 0;
-    *k = -1;
-    char temp[L+1];
-    memset(temp, 0, sizeof(temp));
-    while (caratteri < L) {
-        if((int)buffer[i] >= 33 && (int)buffer[i] <= 133) {
-            if (*k == -1) {
-                *k = i;
+void build_array(Parola** parole, int n_words, char *buff) {
+    char temp[N];
+    free_buff(temp, N);
+    bool is_word = false;
+    int current_n_words = 0;
+    for (int i = 0, j = 0; current_n_words < n_words;) {
+        if (!isEscapeChar(buff[i])) {
+            if (!is_word) {
+                is_word = 1;
             }
-            temp[j] = buffer[i];
-            caratteri++;
-            is_word = 1;
+            temp[j] = buff[i];
+            j++;
         }
-        // creiamo la parola
-        else if( is_word && isEscapeChar(buffer[i]) ) {
-            Parola *p = creaNodo(temp);
-            memset(temp, 0, sizeof(temp));
-            /* Se la testa è null significa che il nodo è il primo della lista */
-            if (testa == NULL) {
-                testa = p;
-                *coda = p;
-            }
-            /* Altrimenti bisogna aggiungere il nodo alla coda */
-            else {
-                (*coda)->next = p;
-                *coda = p;
-            }
-            j = -1;
-            *k = -1;
+        else if (isEscapeChar(buff[i]) && is_word) {
+            Parola *p = crea_nodo(temp);
+            current_n_words++;
+            parole[current_n_words - 1] = p;
+            free_buff(temp, strlen(temp));
+            is_word = false;
+            j = 0;
         }
         i++;
-        j++;
     }
-
-    /* Se ho letto L caratteri e is_word è settata significa che stavo leggendo una parola.
-     * Se incontro un carattere di escape significa che la parola è effettivamente finita e può essere salvata */
-    if ( is_word && isEscapeChar(buffer[i]) ) {
-        Parola *p = creaNodo(temp);
-        if (testa == NULL) {
-            testa = p;
-            *coda = p;
-        }
-            /* Altrimenti bisogna aggiungere il nodo alla coda */
-        else {
-            (*coda)->next = p;
-            *coda = p;
-        }
-        *k = i;
-    }
-    /* Se la parola non è ancora finita si setta il numero di caratteri letti che non formano una parola completa */
-
-    return testa;
 }
 
 /**
- * Alloca uno spazio di memoria per una struttura di tipo Parola e ne ritorna il relativo puntatore all'indirizzo di
- * memoria
- * @param buff stringa da memorizzare all'interno del campo 'parola' della struct
- * @return il puntatore alla zona di memoria dove è stata allocata la struct, NULL in caso di errore
+ * Pulisce un buffer inizializzandolo completamente a 0
+ * @param buff buffer da pulire
+ * @param size dimensione del buffer da pulire
  */
-Parola* creaNodo(char *buff) {
-    Parola *p = (Parola*) malloc(sizeof(Parola));
-    if (p != NULL) {
-        int len = strlen(buff);
-        p->parola = (char *) malloc(len + 1);
-        strncpy(p->parola, buff, len);
-        p->parola[len] = '\0';
-        p->size = len;
-        p->next = NULL;
-    }
-    return p;
+void free_buff(void* buff, int size) {
+    memset(buff, 0, size);
 }
 
 /**
@@ -227,34 +121,140 @@ bool isEscapeChar(char c) {
 }
 
 /**
- * Estrae un nodo dalla testa della lista
- * @param testa la testa della lista dalla quale si vuole estrarre
- * @return il nodo 'Parola' se presente, altrimenti NULL
+ * Alloca uno spazio in memoria per contenere una struttura di tipo 'Parola'
+ * @param buff
+ * @return
  */
-Parola* pop(Parola** testa) {
-    if(*testa == NULL) {
-        return NULL;
-    }
-    Parola* temp = *testa;
-    *testa = (*testa)->next;
-    return temp;
+Parola* crea_nodo(char* buff) {
+    Parola* nodo = NULL;
+    nodo = (Parola *) malloc(sizeof(Parola));
+
+    int len_word = strlen(buff);
+    char* parola = (char*) malloc(len_word);
+    strcpy(parola, buff);
+    nodo->parola = parola;
+    nodo->size = len_word;
+
+    return nodo;
 }
 
 /**
- * Inserisce un nodo 'Parola' all'interno di una lista
- * @param p nodo che si vuole inserire
- * @param testa testa della lista nella quale si vuole inserire la 'Parola'
- * @param coda coda della lista nella quale si vuole inserire la 'Parola'
- * @return la testa della lista
+ * Conta il numero di parole all'interno di un buffer
+ * @param buff buffer dal quale leggere le parole
+ * @return il numero di parole contenute nel buffer
  */
-Parola* push(Parola* p, Parola* testa, Parola** coda) {
-    if (testa == NULL) {
-        testa = p;
-        *coda = testa;
+int count_words(char* buff) {
+    int n_words = 0;
+    bool is_word = false;
+    for (int i = 0; buff[i] != '\0'; ++i) {
+        if (!(isEscapeChar(buff[i]))) {
+            if (!is_word) {
+                is_word = true;
+            }
+        } else if (isEscapeChar(buff[i]) && is_word) {
+            n_words++;
+        }
+    }
+    return n_words;
+}
+
+/**
+ * Si occupa della giustificazione di un testo passato come array di parole
+ * @param parole array di puntattori a struct di tipo 'Parola'
+ * @param n_words numero di parole all'interno dell'array
+ * @param L indica la lunghezza massima della riga
+ */
+void full_Justyfy(Parola** parole, int n_words, int L, Riga *testa, Riga *coda, int *nRighe) {
+    // L'idea è quella di usare due indici che indicano la prima parola da inserire all'interno della riga e la parola da inserire nella riga successiva da inserire all'interno della riga
+    int i = 0;
+    while (i < n_words) {
+        int j = i + 1;
+        // lineLenght: indica la quantità degli effettivi caratteri da stampare
+        int lineLenght = parole[i]->size;
+        check_word_length(parole[i]->parola, L);
+        // j - i - 1: mi fa capire quanti spazi minimi devo inserire
+        while (j < n_words && (lineLenght + parole[j]->size + (j - i - 1) < L) ) {
+            lineLenght += parole[j]->size;
+            j++;
+        }
+
+        int nSpaces = L - lineLenght;
+        int numberOfWords = j - i;
+        // Se la condizione è verificata devo semplicemente fare una giustificazione verso sinistra
+        if (numberOfWords == 1 || j >= n_words) {
+            left_justify(parole, i, j, nSpaces);
+        }
+        else {
+            middle_justify(parole, i, j, nSpaces);
+        }
+        i = j;
+    }
+}
+
+/**
+ * Verifica se una parola può essere contenuta totalmente all'interno di una riga vuota lunga L
+ * @param parola stringa da verificare
+ * @param L lunghezza della riga
+ * @return true se la parola può essere inserita, altrimenti uccide il processo
+ */
+bool check_word_length(char *parola, int L) {
+    if (strlen(parola) > L) {
+        printf("%s è una parola inammissibile: è più lunga di %d caratteri", parola, L);
+        fflush(stdout);     // forzo la scrittura prima di uccidere il processo
+        kill(getpid(), SIGKILL);
+        return false;
+    }
+    return true;
+}
+
+/**
+ * Permette di stampare una stringa allineandola a sinistra. Funziona come una printf classica
+ * @param parole array di parole di tipo 'Parola'
+ * @param i indice della prima parola da stampare
+ * @param j indice dell'ultima parola da stampare
+ * @param numberOfSpaces numero di spazi totali da partizionare all'interno della riga
+ */
+void left_justify(Parola** parole, int i, int j, int numbersOfSpaces, Riga *testa, Riga *coda, int *nRighe) {
+
+    if (j - i == 1) {
+        printf("%s", parole[i]->parola);
+        for (int k = 0; k < numbersOfSpaces; ++k) {
+            printf(" ");
+        }
     }
     else {
-        (*coda)->next = p;
-        *coda = p;
+        for (;i < j ; ++i) {
+            printf("%s", parole[i]->parola);
+            if (i+1 != j) {
+                printf(" ");
+            }
+        }
     }
-    return testa;
+
+    printf("\n");
+}
+
+/**
+ * Si occupa di formattare una riga giustificadola completamente
+ * @param parole array di puntatotori a parole di tipo 'Parola'
+ * @param i indice indicante la posizione nell'array della prima parola della riga da stampare
+ * @param j indice indicante la posizione nell'array della parola successiva all'ultima da stampare
+ * @param numberOfSpaces numero di spazi totali da partizionare all'interno della riga
+ */
+void middle_justify(Parola** parole, int i, int j, int numberOfSpaces, Riga *testa, Riga *coda, int *nRighe) {
+    int spaceNeeded = j - i - 1;                            // numero di spazi minimi necessari (uno tra ogni parola), rappresenta anche in quante parti bisogna dividere gli spazi totali
+    int effectiveSpace = numberOfSpaces / spaceNeeded;      // quanti spazi vanno inseriti tra due parole
+    int extraSpace = numberOfSpaces % spaceNeeded;          // potrebbero dover essere redistribuiti alcuni spazi perchè la quantità di spazi totali è un numero dispari
+
+    for (; i < j; ++i) {
+        printf("%s", parole[i]->parola);
+        for (int k = 0; k < effectiveSpace; ++k) {
+            printf(" ");
+        }
+        if (extraSpace > 0) {
+            printf(" ");
+            extraSpace--;
+        }
+    }
+    printf("\n");
 }
